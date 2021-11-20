2310194d062aba7ab88ca2facd6bfc554ac45b4b
ariana <arianamallquer@gmail.com>
2021-11-20 01:44:15 -0500

    Mostrar vista lista

--- src/main.js
+++ src/main.js
@@ -1,7 +1,48 @@
-import { example } from './data.js';
+//import { example } from './data.js';
+//import athletes from './data/athletes/athletes.js';
 // import data from './data/lol/lol.js';
 import data from './data/athletes/athletes.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
-console.log(data.athletes);
+//console.log(example, data);
+//console.log(data.athletes);
+
+/*for (let i = 0; i < data.athletes.length; i++) {
+    console.log(data.athletes[i].name)//
+    let atletas= document.createElement("h3");
+    let contenido = document.createTextNode("");
+
+    atletas.appendChild(contenido);
+    document.body.appendChild(atletas);
+
+    let contenedor = document.getElementById("container");
+    contenedor.appendChild(atletas);
+}*/
+
+
+
+const sectionCenter = document.getElementById("section-center");
+
+
+    const mostrarAtletas = data.athletes.forEach(function(item){
+        //console.log(item);
+        const createElement = document.createElement("div");
+        const template = `
+        <article class="atletla-item">
+        <div class="item-nombre">
+          <h2>${item.name}</h2>
+        <div class="imagen">
+        <img src="img/usuario.jpg" class="photo" alt="atletla item"/>
+        </div>
+          <h3 class="noc">${item.noc}</h3>
+          <h3 class="sport">${item.sport}</h3>
+        </div>
+        </article>
+      `;
+    
+    createElement.innerHTML=template; 
+    sectionCenter.appendChild(createElement)
+    console.log(template);
+    return template
+});
+
